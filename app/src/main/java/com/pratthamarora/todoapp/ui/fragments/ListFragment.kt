77ade5d44d5a9e4ad0cd76d68b51624fe41016ac
PratthamArora <arora.prattham@gmail.com>
2020-07-18 21:12:38 +0530

    setup update fragment layout

--- app/src/main/java/com/pratthamarora/todoapp/ui/fragments/ListFragment.kt
+++ app/src/main/java/com/pratthamarora/todoapp/ui/fragments/ListFragment.kt
@@ -20,7 +20,9 @@ class ListFragment : Fragment() {
         view.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
-
+        view.listLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
 
         return view
     }
