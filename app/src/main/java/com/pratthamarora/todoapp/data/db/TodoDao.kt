ed3c1e44f7ab4d2428974005903e12d12901e090
PratthamArora <arora.prattham@gmail.com>
2020-07-18 23:50:28 +0530

    setup dao interface

---
+++ app/src/main/java/com/pratthamarora/todoapp/data/db/TodoDao.kt
@@ -0,0 +1,18 @@
+package com.pratthamarora.todoapp.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.pratthamarora.todoapp.data.model.TodoList
+
+@Dao
+interface TodoDao {
+
+    @Query("SELECT * FROM todo_table ORDER BY id ASC")
+    suspend fun getAllData(): LiveData<List<TodoList>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertData(todo: TodoList)
+}
