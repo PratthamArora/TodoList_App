0de07310ccfda3cf2380bc206de8072d4d9b3803
PratthamArora <arora.prattham@gmail.com>
2020-07-19 21:04:53 +0530

    setup data binding for recycler view

--- app/src/main/java/com/pratthamarora/todoapp/ui/adapter/TodoAdapter.kt
+++ app/src/main/java/com/pratthamarora/todoapp/ui/adapter/TodoAdapter.kt
@@ -1,63 +1,39 @@
 package com.pratthamarora.todoapp.ui.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import com.pratthamarora.todoapp.R
-import com.pratthamarora.todoapp.data.model.Priority
 import com.pratthamarora.todoapp.data.model.TodoData
-import com.pratthamarora.todoapp.ui.fragments.ListFragmentDirections
-import kotlinx.android.synthetic.main.todo_item.view.*
+import com.pratthamarora.todoapp.databinding.TodoItemBinding
 
 class TodoAdapter(private var todoList: List<TodoData>) :
     RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
 
-    inner class TodoViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    class TodoViewHolder(private val binding: TodoItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(todo: TodoData) {
+            binding.todoData = todo
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): TodoViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = TodoItemBinding.inflate(layoutInflater, parent, false)
+                return TodoViewHolder(binding)
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-        return TodoViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.todo_item, parent, false)
-        )
+        return TodoViewHolder.from(parent)
     }
 
     override fun getItemCount(): Int = todoList.size
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         val todo = todoList.asReversed()[position]
-        val priority = todo.priority
-        holder.itemView.apply {
-            titleTV.text = todo.title
-            descTV.text = todo.description
-            when (priority) {
-                Priority.HIGH -> priorityIndicator.setCardBackgroundColor(
-                    ContextCompat.getColor(
-                        this.context,
-                        R.color.red
-                    )
-                )
-                Priority.MEDIUM -> priorityIndicator.setCardBackgroundColor(
-                    ContextCompat.getColor(
-                        this.context,
-                        R.color.yellow
-                    )
-                )
-                Priority.LOW -> priorityIndicator.setCardBackgroundColor(
-                    ContextCompat.getColor(
-                        this.context,
-                        R.color.green
-                    )
-                )
-            }
-            itemBG.setOnClickListener {
-                val action = ListFragmentDirections.actionListFragmentToUpdateFragment(
-                    todo = todo
-                )
-                findNavController().navigate(action)
-            }
-        }
+        holder.bind(todo)
     }
 
     fun setList(list: List<TodoData>) {
--- app/src/main/java/com/pratthamarora/todoapp/utils/BindingAdapters.kt
+++ app/src/main/java/com/pratthamarora/todoapp/utils/BindingAdapters.kt
@@ -2,15 +2,19 @@ package com.pratthamarora.todoapp.utils
 
 import android.view.View
 import android.widget.Spinner
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.findNavController
+import com.google.android.material.card.MaterialCardView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.pratthamarora.todoapp.R
 import com.pratthamarora.todoapp.data.model.Priority
+import com.pratthamarora.todoapp.data.model.TodoData
+import com.pratthamarora.todoapp.ui.fragments.ListFragmentDirections
 
 class BindingAdapters {
 
@@ -37,13 +41,49 @@ class BindingAdapters {
 
         @BindingAdapter("parsePriorityToInt")
         @JvmStatic
-        fun parsePriorityToInt(view:Spinner,priority: Priority){
+        fun parsePriorityToInt(view: Spinner, priority: Priority) {
             when (priority) {
                 Priority.HIGH -> view.setSelection(0)
                 Priority.MEDIUM -> view.setSelection(1)
                 Priority.LOW -> view.setSelection(2)
             }
         }
+
+        @BindingAdapter("parsePriorityColor")
+        @JvmStatic
+        fun parsePriorityColor(view: MaterialCardView, priority: Priority) {
+            when (priority) {
+                Priority.HIGH -> view.setCardBackgroundColor(
+                    ContextCompat.getColor(
+                        view.context,
+                        R.color.red
+                    )
+                )
+                Priority.MEDIUM -> view.setCardBackgroundColor(
+                    ContextCompat.getColor(
+                        view.context,
+                        R.color.yellow
+                    )
+                )
+                Priority.LOW -> view.setCardBackgroundColor(
+                    ContextCompat.getColor(
+                        view.context,
+                        R.color.green
+                    )
+                )
+            }
+        }
+
+        @BindingAdapter("sendDataToUpdateFragment")
+        @JvmStatic
+        fun sendDataToUpdateFragment(view: ConstraintLayout, todoData: TodoData) {
+            view.setOnClickListener {
+                val action = ListFragmentDirections.actionListFragmentToUpdateFragment(todoData)
+                view.findNavController().navigate(action)
+            }
+        }
     }
 
 }
+
+
