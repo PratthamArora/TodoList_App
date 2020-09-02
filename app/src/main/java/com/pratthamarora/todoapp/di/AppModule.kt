844af4d0a2aaeff34253ab0ae095d71f9d75e1bc
PratthamArora <arora.prattham@gmail.com>
2020-09-02 20:01:42 +0530

    added app module for dagger

---
+++ app/src/main/java/com/pratthamarora/todoapp/di/AppModule.kt
@@ -0,0 +1,30 @@
+package com.pratthamarora.todoapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.pratthamarora.todoapp.data.db.TodoDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideTodoDatabase(
+        @ApplicationContext app: Context
+    ) = Room.databaseBuilder(
+        app,
+        TodoDatabase::class.java,
+        "todo_database"
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideTodoDao(db: TodoDatabase) = db.toDoDao()
+}
